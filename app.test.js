97d8d7b9b2f03d5f30229417b5294ac08556f0f6
yamamoto <ambmcmdmem@au.com>
2022-04-05 12:21:58 +0900

    テストの命名を修正
    テストケースの追加
    
    Observerクラス　subscribeメソッド　エラーの種類追加

--- app.test.js
+++ app.test.js
@@ -1,9 +1,38 @@
-const observer = require('./modules/observer');
+const Observer = require('./modules/observer');
+const toBeTested = new Observer();
+const forConfirmationOfInteraction = new Observer();
 
-observer.subscribe('output', () => 'test');
-observer.subscribe('output2', (output) => output, 'test2');
+const callbackInvocationToken = [];
+toBeTested.subscribe('push', () => {
+  callbackInvocationToken.push(1);
+  callbackInvocationToken.push('test');
+});
+forConfirmationOfInteraction.subscribe('push', () => {
+  callbackInvocationToken.push(100);
+});
 
 test('Observer test.', () => {
-  expect(observer.publish('output')).toBe('test');
-  expect(observer.publish('output2')).toBe('test2');
+  toBeTested.publish('push');
+  expect(callbackInvocationToken).toStrictEqual([1, 'test']);
+  forConfirmationOfInteraction.publish('push');
+  expect(callbackInvocationToken).toStrictEqual([1, 'test', 100]);
+
+  expect(() => {
+    toBeTested.subscribe();
+  }).toThrow(new Error('イベント名が入力されていません。'));
+  expect(() => {
+    toBeTested.subscribe(1000);
+  }).toThrow(new Error('イベント名の型は文字列としてください。'));
+  expect(() => {
+    toBeTested.subscribe('emptyCallbackArgument');
+  }).toThrow(new Error('コールバック関数が入力されていません。'));
+  expect(() => {
+    toBeTested.subscribe('illegalCallbackArgument', 0);
+  }).toThrow(new Error('コールバック関数が関数ではありません。'));
+  expect(() => {
+    toBeTested.publish();
+  }).toThrow(new TypeError('this.callback[eventName] is not a function'));
+  expect(() => {
+    toBeTested.publish('eventUnregistered');
+  }).toThrow(new TypeError('this.callback[eventName] is not a function'));
 });
--- modules/observer.js
+++ modules/observer.js
@@ -2,12 +2,21 @@ class Observer {
   constructor() {
     this.callback = {};
   }
-  subscribe(eventName, callback, ...callbackArg) {
-    this.callback[eventName] = callback(...callbackArg);
+  subscribe(eventName, callback) {
+    if (typeof eventName === 'undefined')
+      throw new Error('イベント名が入力されていません。');
+    else if (typeof eventName !== 'string')
+      throw new Error('イベント名の型は文字列としてください。');
+    else if (typeof callback === 'undefined')
+      throw new Error('コールバック関数が入力されていません。');
+    else if (typeof callback !== 'function')
+      throw new Error('コールバック関数が関数ではありません。');
+
+    this.callback[eventName] = callback;
   }
   publish(eventName) {
-    return this.callback[eventName];
+    this.callback[eventName]();
   }
 }
 
-module.exports = new Observer();
+module.exports = Observer;
