e6c2379a9e6a8b1de8c65cfaadedc0285542a864
yamamoto <ambmcmdmem@au.com>
2022-04-06 14:39:41 +0900

    Observerクラス　オブジェクトを用いたエラー分岐の削減

--- modules/observer.js
+++ modules/observer.js
@@ -3,10 +3,29 @@ class Observer {
     this.callback = {};
   }
   subscribe(eventName, callback) {
-    if (typeof eventName !== 'string')
-      throw new Error('イベント名の型は文字列としてください。');
-    else if (typeof callback !== 'function')
-      throw new Error('コールバック関数が関数ではありません。');
+    const validityAndErrors = [
+      {
+        validity: typeof eventName === 'undefined',
+        error: new Error('イベント名が入力されていません。'),
+      },
+      {
+        validity: typeof eventName !== 'string',
+        error: new Error('イベント名の型は文字列としてください。'),
+      },
+      {
+        validity: typeof callback === 'undefined',
+        error: new Error('コールバック関数が入力されていません。'),
+      },
+      {
+        validity: typeof callback !== 'function',
+        error: new Error('コールバック関数が関数ではありません。'),
+      },
+    ];
+
+    if (validityAndErrors.some((validityAndError) => validityAndError.validity))
+      throw validityAndErrors.find(
+        (validityAndError) => validityAndError.validity
+      ).error;
 
     this.callback[eventName] = callback;
   }
